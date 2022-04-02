e639f0866d74e92740b97b4691955354b6b6be31
Septianleonardo <dwigantoroleonardoseptian@gmail.com>
2022-04-02 09:09:18 +0700

    menambahkan fungsi route menampilkan 1 user

--- routes/users.js
+++ routes/users.js
@@ -43,7 +43,14 @@ router.delete("/:id", async (req, res) => {
 });
 
 // menampilkan satu user
-
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // follow user
 
 // unfollow user
