2355fd2b099b8df7eb3f32e691f2b24eb30d3cf1
Septianleonardo <dwigantoroleonardoseptian@gmail.com>
2022-05-07 21:02:36 +0700

    deployment kelima PadjadjaranHub; perbaikan asset localhost

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,11 +1,11 @@
-import axios from "axios";
 import {LoginSuccess} from "./context/AuthAction";
+import { axiosInstance } from "./config";
 
 // ketika melakukan request untuk login
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("auth/login", userCredential);
+    const res = await axiosInstance.post("auth/login", userCredential);
     dispatch(LoginSuccess(res.data));
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
