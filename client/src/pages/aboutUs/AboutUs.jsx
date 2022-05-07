7bd18e6e2dda72c6f6000395bd943b25aeb91377
AzharJau <jauharulumamazhar@gmail.com>
2022-05-07 14:57:39 +0700

    aboutUs

---
+++ client/src/pages/aboutUs/AboutUs.jsx
@@ -0,0 +1,25 @@
+import "./aboutUs.css"
+export default function AboutUs() {
+  return (
+    <>
+          <div className="titleAboutus">OUR Team</div>
+<div className="grid-container">
+  <div>Project Manager</div>
+  <div>UI/UX Designer</div>
+  <div>FrontEnd</div>
+  <div>BackEnd</div>
+  <div><img src="/assets/22.jpg" alt="" className="imgAboutus"/> </div>
+  <div><img src="/assets/74.jpg" alt="" className="imgAboutus"/></div>
+  <div><img src="/assets/30.jpg" alt="" className="imgAboutus"/></div>  
+  <div><img src="/assets/38.jpg" alt="" className="imgAboutus"/></div>
+  <div>M Diva Eka Andriana <br />140810190022</div>
+  <div>Fahrul Thariq Fadillah <br />140810190074</div>  
+  <div>Azhar Jauharul Umam <br />140810190030</div>
+  <div>Leonardo Septian Dwigantoro <br />140810190038</div>
+</div>
+
+  
+        <div className="footerAboutus"></div>
+    </>
+  );
+}
