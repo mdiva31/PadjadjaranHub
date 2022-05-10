ad24eb91ffa9fed1a2b61dcae4482445a5014ffe
Septianleonardo <dwigantoroleonardoseptian@gmail.com>
2022-05-10 09:59:55 +0700

    perbaikan helmet dan package

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // middleware
 app.use(express.json());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+);
 app.use(morgan("common"));
 
 // destinasi dari upload file
